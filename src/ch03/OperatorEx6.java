151a183acd0be9c52b270c7ab481971b3134bd92
kdo <kdo_1999@naver.com>
2023-05-24 20:58:33 +0900

    Initial commit

---
+++ src/ch03/OperatorEx6.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class OperatorEx6 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+//        byte c = a + b; //컴파일 에러 발생, 명시적으로 형변환이 필요하다. byte c = (byte) (a + b);
+        byte c = (byte) (a + b);
+
+        System.out.println(c);
+    }
+}
