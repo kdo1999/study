fb29bed1749023078bbd64cd284330ad39036777
kdo <kdo_1999@naver.com>
2023-05-24 20:58:33 +0900

    Initial commit

---
+++ src/ch01/Hello.java
@@ -0,0 +1,12 @@
+package ch01;
+/*
+* Date : 2023. 05. 24
+* Source : Hello.java
+* Author : 김동오
+* Email : kdo_1999@naver.com
+* */
+public class Hello {
+    public static void main(String[] args) { /* 프로그램의 시작 */
+        System.out.println("Hello, world."); // Hello, world 출력
+    }
+}
